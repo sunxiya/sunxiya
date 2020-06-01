0ff123e89f0cfc082c36dd4ac20da2a28b4d4ea2
sunxiya <1693566138@qq.com>
2020-06-01 16:59:30 +0800

    单元测试实验 first commit

---
+++ sunxiya/src/sizeyunsan/xiaoxueshengTest.java
@@ -0,0 +1,49 @@
+package sizeyunsan;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class xiaoxueshengTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testReductionofFraction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBiaodashi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+}
